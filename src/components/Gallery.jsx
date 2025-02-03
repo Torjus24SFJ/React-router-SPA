81f6655ec1412579265d8972e34da849312c3b57
Torjus <Torjus24sfj@gmail.com>
2025-02-03 14:16:59 +0100

    shuffle

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,33 +1,44 @@
 import { useState, useEffect } from "react";
 import style from "./Gallery.module.css";
+import useSWR from "swr";
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
 export const Gallery = () => {
+const {data, error, isLoading} = useSWR("https://restcountries.com/v2/all", fetcher);
+
 const [countries, setCountries] = useState([]);
 
-    useEffect(() => {
-        const fetchImages = async() => {
-            try {
-                const response = await fetch("https://restcountries.com/v2/all")
-                const countries = await response.json();
-                setCountries(countries.splice(0, 4));
-            }
-            catch(error) {
-                console.log("Error!", error)
-            }
-        };
-        fetchImages();
-    },[]);
+useEffect(() => {
+  if (data && countries.length === 0) {
+    setCountries(data.slice(0, 8));
+  }
+}, [data, countries.length]);
 
-  return (
-    <>
-      <h1 className="title_header_text">This is the gallery!</h1>
-      <div className="main_content_container">
-        <div className={style.grid_gallery_container}>
-            {countries.map((countries, index) => (
-                <div key={index} className={style.picture_card}><img src={countries.flags.png} alt={countries.name}/></div>
+
+const shuffleCountries = () => {
+  if (data) {
+    const shuffled = [...data].sort(() => Math.random() - 0.5).slice(0, 8);
+    setCountries(shuffled);
+  }
+}
+
+if(isLoading) return <div>Loading...</div>
+if(error) return <div>Error loading data</div>
+
+
+    return (
+      <>
+        <h1 className="title_header_text">This is the gallery!</h1>
+        <div className="main_content_container">
+          <button className={style.randomize_button} onClick={shuffleCountries}>Randomize</button>
+          <div className={style.grid_gallery_container}>
+            {countries.map((country, index) => (
+              <div key={index} className={style.picture_card}>
+                <img src={country.flags.png} alt={country.name} />
+              </div>
             ))}
+          </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  };
