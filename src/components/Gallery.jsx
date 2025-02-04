03e723fbb751b3dbccd928c29255c321d2d8825c
Torjus <Torjus24sfj@gmail.com>
2025-02-04 11:21:37 +0100

    adding check if data is already loading

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,27 +11,44 @@ export const Gallery = () => {
 
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [region, setRegion] = useState("");
 
-   useEffect(() => {
+  //! Setting init countries
+  useEffect(() => {
     if (data && countries.length === 0) {
       setCountries(data.slice(0, 8));
     }
   }, [data, countries.length]);
 
+  //! Setting countries based on searchTerm
   useEffect(() => {
-    if (searchTerm === "") {
-      setCountries(data.slice(0, 8))
-    } else {
-      const filtered = data.filter((country) => 
-        country.name.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      setCountries(filtered);
+    if (!isLoading) {
+      if (searchTerm === "") {
+        setCountries(data.slice(0, 8));
+      } else {
+        const filtered = data.filter((country) =>
+          country.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setCountries(filtered);
+      }
     }
-  }, [searchTerm, data]);
+  }, [searchTerm, data, isLoading]);
 
+  //! Setting countries based on regions
+  useEffect(() => {
+    if(!isLoading){
+      if (region === "") {
+        setCountries(data.slice(0, 8));
+      } else {
+        const filtered = data.filter((country) => country.region === region);
+        setCountries(filtered);
+      }
+    }
+  }, [region, data, isLoading]);
 
+  //! Setting random countries on button press
   const shuffleCountries = () => {
-    if (data) {
+    if (!isLoading) {
       const shuffled = [...data].sort(() => Math.random() - 0.5).slice(0, 8);
       setCountries(shuffled);
     }
@@ -42,7 +59,6 @@ export const Gallery = () => {
 
   return (
     <>
-      <h1 className="title_header_text">This is the gallery!</h1>
       <div className={style.top_buttons_flex}>
         <input
           type="text"
@@ -54,6 +70,18 @@ export const Gallery = () => {
         <button className={style.randomize_button} onClick={shuffleCountries}>
           Randomize
         </button>
+      </div>
+      <div className={style.filtered_button}>
+        <label>Filter by Region</label>
+        <br />
+        <select onChange={(e) => setRegion(e.target.value)} value={region}>
+          <option value="">All</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
       </div>
       <div className="main_content_container">
         <div className={style.grid_gallery_container}>
